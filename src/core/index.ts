a820cb17938f09ed5f9f2796b53cded0ae1d907b
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-05-27 21:52:15 +0000

    SDK regeneration

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,5 +1,4 @@
 export * from "./fetcher";
-export * from "./runtime";
-export * from "./auth";
 export * from "./form-data-utils";
+export * from "./runtime";
 export * as serialization from "./schemas";
